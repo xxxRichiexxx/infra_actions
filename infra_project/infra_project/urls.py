3f07692ff1c9a873c9ee1b928c080666cf96fb1c
xxxRichiexxx <xxxRichiexxx@yandex.ru>
2022-04-30 12:24:41 +0300

    add test with flake8 v6

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
